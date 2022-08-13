4a381b4422c26b0d05767eca1d0bbf6eedd2d42b
Diana Kwamboka <diana.kwamboka@student.moringaschool.com>
2022-08-13 22:16:15 +0300

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Write your solution here!
+//expected.to.have.ordered.members 
+function (cats); {
+    let names = ["Milo", "Otis", "Garfield"];
+
+}
+function length{
+    cats.length = 0;
+    cats.push ("Milo", "Otis", "Garfield");
+
+}
+function append{
+    cats.unshift("Ralph");
+
+}
+function prepend{
+    cats.splice(1,0 "Bob");
+
+}
+
