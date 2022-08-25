b042d09a02686a704052bf2aaa90ec41e9ccaf96
Diana Kwamboka <diana.kwamboka@student.moringaschool.com>
2022-08-25 11:50:25 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,19 +1,57 @@
 //expected.to.have.ordered.members 
-function (cats); {
-    let names = ["Milo", "Otis", "Garfield"];
+const cats = ["Milo","Otis","Garfield"]
 
-}
-function length{
-    cats.length = 0;
-    cats.push ("Milo", "Otis", "Garfield");
 
+function destructivelyAppendCat(name){
+    return cats.push(name);    
 }
-function append{
-    cats.unshift("Ralph");
 
-}
-function prepend{
-    cats.splice(1,0 "Bob");
 
+function destructivelyPrependCat(name){
+    return cats.unshift(name)
 }
+
+
+function destructivelyRemoveLastCat(name){
+    return cats.pop(name)
+}
+
+
+function destructivelyRemoveFirstCat(name){
+    return cats.shift(name)
+}
+
+
+function appendCat(name){
+    let newCats = [...cats, name];
+    return newCats;
+    
+}
+appendCat('Broom');
+console.log(cats);
+
+
+function prependCat(name){
+    let newCats = [name, ...cats];
+    return newCats;
+
+    console.log(newCats);
+}
+
+
+function removeLastCat(){
+    let newCats=cats.slice(0,2);
+    return newCats;
+
+    console.log(newCats);
+}
+
+
+function removeFirstCat(){
+    let newCats=cats.slice(1)
+    return newCats;
+
+    console.log(newCats)
+}
+
 
